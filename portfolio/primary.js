17ebaaf2fe474809e78569fac33ea3dc534870cc
Dylan Sieren <dylan.sieren01@gmail.com>
2018-11-25 20:13:39 -0600

    added some animations for the projects page

--- portfolio/primary.js
+++ portfolio/primary.js
@@ -1,7 +1,10 @@
 
 const links = document.getElementsByTagName("a");
 const scrollButton = document.querySelector('.scroll-button');
-
+const projectDescriptions = document.querySelectorAll(".project-description");
+const smallProjectContainers = document.querySelectorAll(".small-project-container");
+const bigProjectContainers = document.querySelector(".big-project-container");
+const projectDescriptionEven = document.querySelector(".project-cards:nth-child(even) .project-description");
 
 //runs the function WindowScroll onscroll
 window.onscroll =  () => {
@@ -21,33 +24,49 @@ const windowScroll = () => {
   
   const animateSkills = document.querySelectorAll(".skills");
   const yPosition = window.pageYOffset;
+
   for (let i = 0; i < animateSkills.length; i++) {
     if (yPosition > 80) {
       animateSkills[i].classList.add("fall-in-animation");
     }
   }
-  //stores the windows scroll movment into the document element, document object.
-  const scrolled = document.documentElement.scrollTop;
-  //allows these three different elelements to be moved with the window scroll at different speeds.
 
-    document.querySelector('.intro-container').style.top = 0 - scrolled * .25 + "px";
-    document.querySelector('.first-name').style.top = 0 - scrolled * .20 + "px";
-    document.querySelector('.last-name').style.top = 400 - scrolled * .10 + "px";
-    document.querySelector('.image-container').style.top = 0 - scrolled * .05 + "px";
-    document.querySelector('.contact-link-block').style.top = 0 + scrolled * .05 + "px";
+  for(let i = 0; i < projectDescriptions.length; i++ ){
+    if(yPosition > 1000) {
+      projectDescriptions[i].classList.add("slide-in-from-left");
+      projectDescriptions[i].style.left = "100px";
+    }
+  }
+
+  for(let i = 0; i < smallProjectContainers.length; i++ ){
+    if(yPosition > 1000) {
+      smallProjectContainers[i].classList.add("slide-in-from-right");
+      bigProjectContainers.classList.add("slide-in-from-left");
+      projectDescriptionEven.classList.add("slide-in-from-right");
+    }
+  }
+
+
+  projectDescriptionEven.style.left = "-100px";
+  const scrolled = document.documentElement.scrollTop;
+    //allows these three different elelements to be moved with the window scroll at different speeds.
+    // document.querySelector('.project-cards:nth-child(even) .project-description').style.top = 0 - scrolled * .40 + "px";
+  document.querySelector('.intro-container').style.top = 0 - scrolled * .25 + "px";
+  document.querySelector('.code-word').style.top = 0 - scrolled * .20 + "px";
+  document.querySelector('.last-name').style.top = 400 - scrolled * .10 + "px";
+  document.querySelector('.image-container').style.top = 0 - scrolled * .05 + "px";
+  document.querySelector('.contact-link-block').style.top = 0 + scrolled * .05 + "px";
 };
 
-//grabs the element .toggle-mnu and adds an event list
 document.querySelector(".toggle-mnu").addEventListener("click",toggleMenu);
 
 function toggleMenu() {
   this.classList.toggle ("toggle-active");
-document.querySelector(".main-ul").classList.toggle("on");
+  document.querySelector(".main-ul").classList.toggle("on");
 }
 
-// storing the card-1 element into a variable to use in a mousemove function
+
 const parallaxContainer = document.querySelector('#card-1');
-  //create a function and attach an eventlister that listens for mouse movement
 parallaxContainer.addEventListener ("mousemove", function(e) {
   //storing the windows outer width into a variable so we can use it in our mousemove function
   let screenWidth = window.outerWidth;
@@ -55,8 +74,16 @@ parallaxContainer.addEventListener ("mousemove", function(e) {
   let screenHeight = window.outerHeight;
   // allows the image-container element to move with the mouse
   document.querySelector(".image-container").style.transform = 'translate(-' + e.pageX/screenWidth * 10 + 'px, -' + e.pageY/screenHeight * 10 + "px)";
-  document.querySelector(".first-name-container").style.transform = "translate(" + e.pageX/screenWidth * 15 + "px, " + e.pageY/screenHeight * 15 + "px)";
+  document.querySelector(".code-word-container").style.transform = "translate(" + e.pageX/screenWidth * 15 + "px, " + e.pageY/screenHeight * 15 + "px)";
   document.querySelector(".last-name").style.transform = "translate(" + e.pageX/screenWidth * 15 + "px, " + e.pageY/screenHeight * 15 + "px)";
   document.querySelector(".intro-container").style.transform = "translate(" + e.pageX/screenWidth * 15 + "px, " + e.pageY/screenHeight * 15 + "px)";
   document.querySelector(".contact-link-block").style.transform = "translate(" + e.pageX/screenWidth * 15 + "px, " + e.pageY/screenHeight * 15 + "px)";
 });
+
+function homePageAnimations() {
+  
+}
+
+windowScroll.onload = () => {
+
+}
