98a22df1032f36cd3799f0dd75a2833ebf466fd7
Dylan <dylan.sieren01@gmail.com>
2018-11-27 23:14:53 -0600

    cleaned up javascript code a bit and made it more performant

--- portfolio/primary.js
+++ portfolio/primary.js
@@ -1,33 +1,39 @@
-const links = document.getElementsByTagName("a");
+
 const scrollButton = document.querySelector(".scroll-button");
 const projectDescriptions = document.querySelectorAll(".project-description");
 const smallProjectContainers = document.querySelectorAll(".small-project-container");
 const bigProjectContainers = document.querySelector(".big-project-container");
 const projectDescriptionEven = document.querySelector(".project-cards:nth-child(even) .project-description");
 const contactMe = document.querySelector(".card-4-container");
+const parallaxContainer = document.getElementById("#card-1");
+const cardOneButton = document.querySelector(".contact-link-block");
+const cardOneImage =   document.querySelector(".image-container");
+const firstLargeCardOneWord =   document.querySelector(".code-word");
+const firstLargeCardOneWordContainer =   document.querySelector(".code-word-container");
+const secondLargeCardOneWord =   document.querySelector(".last-name");
+const hook =   document.querySelector(".intro-container");
 
-//runs the function WindowScroll onscroll
+
 window.onscroll = () => {
   windowScroll();
 };
+
 const windowScroll = () => {
   const animateSkills = document.querySelectorAll(".skills");
+  const mainNav = document.querySelector(".main-nav");
   const yPosition = window.pageYOffset;
   const scrolled = document.documentElement.scrollTop;
-  const mainNav = document.querySelector(".main-nav");
-  mainNav.classList.toggle(
-    //if mainNav does not have an attribute of scrolled-nav-background then it turns true and runs adds the class "scrolled-nav-background"
-    "scrolled-nav-background",
-    //mainNav is set to have the background set to true when it is scrolled > 600
-    mainNav.scrollTop / screen.height > 0.75 ||
-      document.documentElement.scrollTop / screen.height > 0.75
+
+
+  mainNav.classList.toggle( "scrolled-nav-background", mainNav.scrollTop / screen.height > 0.75 
+    || document.documentElement.scrollTop / screen.height > 0.75
   );
+
   for (let i = 0; i < animateSkills.length; i++) {
     if (yPosition > 80) {
       animateSkills[i].classList.add("fall-in-animation");
     }
   }
-
   for (let i = 0; i < projectDescriptions.length; i++) {
     if (yPosition > 900) {
       projectDescriptions[i].classList.add("slide-in-from-left");
@@ -46,26 +52,24 @@ const windowScroll = () => {
   }
   projectDescriptionEven.style.left = "-100px";
   //allows these three different elelements to be moved with the window scroll at different speeds.
-  document.querySelector(".intro-container").style.top =0 - scrolled * 0.25 + "px";
-  document.querySelector(".code-word").style.top = 0 - scrolled * 0.2 + "px";
-  document.querySelector(".last-name").style.top = 400 - scrolled * 0.1 + "px";
-  document.querySelector(".image-container").style.top =0 - scrolled * 0.05 + "px";
-  document.querySelector(".contact-link-block").style.top =0 + scrolled * 0.05 + "px";
+  hook.style.top =0 - scrolled * 0.25 + "px";
+  firstLargeCardOneWord.style.top = 0 - scrolled * 0.2 + "px";
+  secondLargeCardOneWord.style.top = 400 - scrolled * 0.1 + "px";
+  cardOneImage.style.top =0 - scrolled * 0.05 + "px";
+  cardOneButton.style.top =0 + scrolled * 0.05 + "px";
 };
 
 // ****** Fires mouseover Parallax effect ******//
-const parallaxContainer = document.querySelector("#card-1");
 parallaxContainer.addEventListener("mousemove", e => {
-  //storing the windows outer width into a variable so we can use it in our mousemove function
   let screenWidth = window.outerWidth;
-  //storing the windows outer height into a variable so we can use it in our mousemove function
   let screenHeight = window.outerHeight;
-  // allows the image-container element to move with the mouse
-  document.querySelector(".image-container").style.transform ="translate(-" +(e.pageX / screenWidth) * 10 +"px, -" +(e.pageY / screenHeight) * 10 +"px)";
-  document.querySelector(".code-word-container").style.transform ="translate(" +(e.pageX / screenWidth) * 15 +"px, " +(e.pageY / screenHeight) * 15 +"px)";
-  document.querySelector(".last-name").style.transform ="translate(" +(e.pageX / screenWidth) * 15 +"px, " +(e.pageY / screenHeight) * 15 +"px)";
-  document.querySelector(".intro-container").style.transform ="translate(" +(e.pageX / screenWidth) * 15 +"px, " +(e.pageY / screenHeight) * 15 +"px)";
-  document.querySelector(".contact-link-block").style.transform ="translate(" +(e.pageX / screenWidth) * 15 +"px, " +(e.pageY / screenHeight) * 15 +"px)";
+
+  // Elements move based on cursor movement
+  cardOneImage.style.transform ="translate(-" +(e.pageX / screenWidth) * 10 +"px, -" +(e.pageY / screenHeight) * 10 +"px)";
+  firstLargeCardOneWordContainer.style.transform ="translate(" +(e.pageX / screenWidth) * 15 +"px, " +(e.pageY / screenHeight) * 15 +"px)";
+  secondLargeCardOneWord.style.transform ="translate(" +(e.pageX / screenWidth) * 15 +"px, " +(e.pageY / screenHeight) * 15 +"px)";
+  hook.style.transform ="translate(" +(e.pageX / screenWidth) * 15 +"px, " +(e.pageY / screenHeight) * 15 +"px)";
+  cardOneButton.style.transform ="translate(" +(e.pageX / screenWidth) * 15 +"px, " +(e.pageY / screenHeight) * 15 +"px)";
 });
 
 document.querySelector(".toggle-mnu").addEventListener("click", toggleMenu);
