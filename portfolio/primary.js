6419d4642ef9332ded97dd42d4d04082fcb1c85f
Dylan Sieren <dylan.sieren01@gmail.com>
2018-12-03 19:36:24 -0600

    cleaned up my JavaScript code a little bit. Still see room for improvement

--- portfolio/primary.js
+++ portfolio/primary.js
@@ -1,4 +1,3 @@
-
 const scrollButton = document.querySelector(".scroll-button");
 const projectDescriptions = document.querySelectorAll(".project-description");
 const smallProjectContainers = document.querySelectorAll(".small-project-container");
@@ -12,7 +11,13 @@ const firstLargeCardOneWord = document.querySelector(".code-word");
 const firstLargeCardOneWordContainer = document.querySelector(".code-word-container");
 const secondLargeCardOneWord = document.querySelector(".last-name");
 const hook = document.querySelector(".intro-container");
+const xPosition = window.innerWidth;
 
+document.querySelector(".hamburger-menu").addEventListener("click", toggleMenu);
+function toggleMenu() {
+  this.classList.toggle("toggle-active");
+  document.querySelector(".main-ul").classList.toggle("on");
+}
 
 window.onscroll = () => {
   windowScroll();
@@ -26,23 +31,38 @@ const windowScroll = () => {
   const yPosition = window.pageYOffset;
   const scrolled = document.documentElement.scrollTop;
 
+  // Scroll Parallax for my projects
+  for (let i = 0; i < projectDescriptions.length; i++) {
+    projectDescriptions[0].style.top = -80 + scrolled * 0.04 + "px";
+    projectDescriptions[1].style.top = -110 + scrolled * 0.06 + "px";
+    projectDescriptions[2].style.top = -120 + scrolled * 0.08 + "px";
+    bigProjectContainers.style.top = -100 - scrolled * 0.06 + "px";
+    smallProjectContainers[0].style.top = -100 - scrolled * 0.05 + "px";
+    smallProjectContainers[1].style.top = -100 - scrolled * 0.06 + "px";
+    hook.style.top = 0 - scrolled * 0.25 + "px";
+    firstLargeCardOneWord.style.top = 0 - scrolled * 0.2 + "px";
+    secondLargeCardOneWord.style.top = 400 - scrolled * 0.1 + "px";
+    cardOneImage.style.top = 0 - scrolled * 0.05 + "px";
+    cardOneButton.style.top = 0 + scrolled * 0.05 + "px";
+  }
 
-  mainNav.classList.toggle( "scrolled-nav-background", mainNav.scrollTop / screen.height > 0.75 
-    || document.documentElement.scrollTop / screen.height > 0.75
+  //Main Navigation Scrolled Background
+  mainNav.classList.toggle(
+    "scrolled-nav-background",
+    mainNav.scrollTop / screen.height > 0.75 ||
+      document.documentElement.scrollTop / screen.height > 0.75
   );
-  if(mainNav.classList.contains("scrolled-nav-background")){
-    logo.style.color ="#fff";
-    for(let i = 0; i < mainLinks.length; i++) {
+  if (mainNav.classList.contains("scrolled-nav-background")) {
+    logo.style.color = "#fff";
+    for (let i = 0; i < mainLinks.length; i++) {
       mainLinks[i].style.color = "#fff";
     }
-
   } else {
     logo.style.color = "#000";
-    for(let i = 0; i < mainLinks.length; i++) {
+    for (let i = 0; i < mainLinks.length; i++) {
       mainLinks[i].style.color = "#000";
     }
   }
-
 
   // Animates the Skills section, Project section, and about me container.
   for (let i = 0; i < animateSkills.length; i++) {
@@ -50,42 +70,27 @@ const windowScroll = () => {
       animateSkills[i].classList.add("fall-in-animation");
     }
   }
-  
-  for (let i = 0; i < projectDescriptions.length; i++) {
-    projectDescriptions[0].style.top = -80 + scrolled *.04 + "px";
-    projectDescriptions[1].style.top = -110 + scrolled *.06 + "px";
-    projectDescriptions[2].style.top = -120 + scrolled *.08 + "px";
-    bigProjectContainers.style.top = -100 - scrolled * .06 + "px";
-    smallProjectContainers[0].style.top = -100 - scrolled * .05 + "px";
-    smallProjectContainers[1].style.top = -100 - scrolled * .06 + "px";
 
-    if(yPosition >= 700) {
+  for (let i = 0; i < projectDescriptions.length; i++) {
+    if (yPosition >= 700) {
       smallProjectContainers[0].classList.add("rotateInUpRight");
       projectDescriptions[i].style.left = "100px";
       projectDescriptions[0].classList.add("rotateInUpLeft");
-  } 
-    if(yPosition >= 1200) {
+    }
+    if (yPosition >= 1200) {
       bigProjectContainers.classList.add("rotateInUpLeft");
       projectDescriptionEven.classList.add("rotateInUpRight");
-    }        
+    }
     if (yPosition >= 1500) {
       smallProjectContainers[1].classList.add("rotateInUpRight");
       projectDescriptions[2].classList.add("rotateInUpLeft");
+    }
   }
 
-  }
   if (yPosition > 2000) {
     contactMe.classList.add("fadeInUp");
   }
   projectDescriptionEven.style.left = "-100px";
-
-  //allows these three different elelements to be moved with the window scroll at different speeds.
-  hook.style.top = 0 - scrolled * 0.25 + "px";
-  firstLargeCardOneWord.style.top = 0 - scrolled * 0.2 + "px";
-  secondLargeCardOneWord.style.top = 400 - scrolled * 0.1 + "px";
-  cardOneImage.style.top = 0 - scrolled * 0.05 + "px";
-  cardOneButton.style.top = 0 + scrolled * 0.05 + "px";
-
 };
 
 // // ****** Fires mouseover Parallax effect ******//
@@ -100,9 +105,3 @@ const windowScroll = () => {
 //   hook.style.transform ="translate(" +(e.pageX / screenWidth) * 15 +"px, " +(e.pageY / screenHeight) * 15 +"px)";
 //   cardOneButton.style.transform ="translate(" +(e.pageX / screenWidth) * 15 +"px, " +(e.pageY / screenHeight) * 15 +"px)";
 // });
-
-document.querySelector(".hamburger-menu").addEventListener("click", toggleMenu);
-function toggleMenu() {
-  this.classList.toggle("toggle-active");
-  document.querySelector(".main-ul").classList.toggle("on");
-}
