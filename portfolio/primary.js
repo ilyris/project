42d75b1d0a16724b179c2d5de93197881e22523d
Dylan Sieren <dylan.sieren01@gmail.com>
2019-07-03 02:59:09 -0500

    fixed my mobile menu, about me area

--- portfolio/primary.js
+++ portfolio/primary.js
@@ -11,84 +11,49 @@ const cardOneImage = document.querySelector('.image-container');
 const firstLargeCardOneWord = document.querySelector('.code-word');
 const firstLargeCardOneWordContainer = document.querySelector('.code-word-container');
 const secondLargeCardOneWord = document.querySelector('.last-name');
-const hook = document.querySelector('.intro-container');
 const mainNav = document.querySelector('.main-nav');
 const logo = document.querySelector('.logo');
 const mainLinks = document.querySelectorAll('.main-links');
 const animateSkills = document.querySelectorAll('.skills');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
+// const hamburgerMenu = document.querySelector('.hamburger-menu');
+document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu, false);
 
-
-hamburgerMenu.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-  this.classList.toggle('toggle-active');
-  document.querySelector('.main-ul').classList.toggle('on');
-}
 
 window.onscroll = () => {
   animateElementsBasedOnWindowMeasurements();
   setTopOfElements();
   toggleScrolledNavigationBackground();
 };
+function toggleMenu() {
+  this.classList.toggle("toggle-active");
+  const hamburgerMenu = document.querySelector('.hamburger-menu').classList;
+  const wrapper = document.getElementById('mobileMenuWrapper').classList;
 
+  if(hamburgerMenu.contains("toggle-active")) {
+    wrapper.add("mobileOn");
+    wrapper.toggle("translateUp");
+  } else if(!hamburgerMenu.contains('toggle-active')) {
+    wrapper.remove("translateUp");
+  }
+
+}
 function setTopOfElements() {
    const scrolled = document.documentElement.scrollTop;
 
-      // projectDescriptions[0].style.top = -80 + scrolled * 0.04 + 'px';
-    // projectDescriptions[1].style.top = -110 + scrolled * 0.06 + 'px';
-    // projectDescriptions[2].style.top = -120 + scrolled * 0.08 + 'px';
-    // bigProjectContainers.style.top = -100 - scrolled * 0.06 + 'px';
-    // smallProjectContainers[0].style.top = -100 - scrolled * 0.05 + 'px';
-    // smallProjectContainers[1].style.top = -100 - scrolled * 0.06 + 'px';
-    // hook.style.top = 0 - scrolled * 0.25 + 'px';
-    // firstLargeCardOneWord.style.top = 0 - scrolled * 0.2 + 'px';
-    // secondLargeCardOneWord.style.top = 400 - scrolled * 0.1 + 'px';
-    // cardOneImage.style.top = 0 - scrolled * 0.05 + 'px';
-    // cardOneButton.style.top = 0 + scrolled * 0.05 + 'px';
 
     // Scroll Parallax for my projects
     const offsets = [-80, -110, -120];
-    // you can also omit the return statement and the curly braces in these cases
     const projectScrollParallaxRate = i => 0.04 + 0.02 * i;
 
     for (let i = 0; i < projectDescriptions.length; i++) {
-      // AR - here and throughout your code we could use a template literal in place of string concatenation
-      // I think they are easier to read, and then you don't have to worry about escaping quotes (not relevant in this case)
       projectDescriptions[i].style.top =  offsets[ i ]  + scrolled * projectScrollParallaxRate( i ) + 'px';
     }
 
     bigProjectContainers.style.top = -100 - scrolled * 0.06 + 'px';
     smallProjectContainers[0].style.top = -100 - scrolled * 0.05 + 'px';
     smallProjectContainers[1].style.top = -100 - scrolled * 0.06 + 'px';
-    hook.style.top = 0 - scrolled * 0.25 + 'px';
-    firstLargeCardOneWord.style.top = 0 - scrolled * 0.2 + 'px';
-    secondLargeCardOneWord.style.top = 400 - scrolled * 0.1 + 'px';
-    cardOneImage.style.top = 0 - scrolled * 0.05 + 'px';
-    cardOneButton.style.top = 0 + scrolled * 0.05 + 'px';
 }
 
-const toggleScrolledNavigationBackground = () => {
-  mainNav.classList.toggle(
-    'scrolled-nav-background',  
-    // AR - it might help to encapsulate these values into a variable for easier reference and reading
-    // meaning, it's not immediately obvious to me what this arithmetic is accomplishing
-    mainNav.scrollTop / screen.height > 0.75 ||
-      document.documentElement.scrollTop / screen.height > 0.75 // put numbers into variable for future reference. ******
-  );
-  if (mainNav.classList.contains('scrolled-nav-background')) {
-    logo.style.color = '#fff';
-
-    mainLinks.forEach( (link) => {
-      link.style.color ="#fff";
-    })
-   } else {
-    logo.style.color = '#000';
-    for (let i = 0; i < mainLinks.length; i++) {
-      mainLinks[i].style.color = '#000';
-    }
-  }
-}
 
   // Scroll Parallax for my projects
 const animateElementsBasedOnWindowMeasurements = () => {
@@ -103,7 +68,6 @@ const animateElementsBasedOnWindowMeasurements = () => {
     }
   }
 
-  // AR - have you considered using a switch statement here?
   if (yPosition >= 700 ) {
     smallProjectContainers[0].classList.add('fadeInRight');
     projectDescriptions[0].classList.add('fadeInLeft');
@@ -121,6 +85,28 @@ const animateElementsBasedOnWindowMeasurements = () => {
   }
 }
 
+const toggleScrolledNavigationBackground = () => {
+  const mainNav = document.getElementById("menuBarWrapper");
+  const mainLinks = document.getElementsByClassName("menuLinkAnchors");
+  const logo = document.querySelector("#logoContainer .logo a");
+  mainNav.classList.toggle(
+    "scrolled-nav-background",
+    mainNav.scrollTop / screen.height > 0.75 ||
+      document.documentElement.scrollTop / screen.height > 0.75
+  );
+    if(mainNav.classList.contains('scrolled-nav-background') === true) {
+      logo.classList.add('activated-scrolled-links');
+        for(let i = 0; i < mainLinks.length; i++) {
+          mainLinks[i].classList.add('activated-scrolled-links');
+        }
+    } else if(mainNav.classList.contains('scrolled-nav-background') === false) {
+      logo.classList.remove('activated-scrolled-links');
+        for(let i = 0; i < mainLinks.length; i++) {
+          mainLinks[i].classList.remove('activated-scrolled-links');
+      }
+    }
+};
+
 //const mouseMoveParallax = () => {
 // // ****** Fires mouseover Parallax effect ******//
 // const parallaxContainer = document.getElementById('card-1');
@@ -136,4 +122,5 @@ const animateElementsBasedOnWindowMeasurements = () => {
 //   cardOneButton.style.transform ='translate(' +(e.pageX / screenWidth) * 15 +'px, ' +(e.pageY / screenHeight) * 15 +'px)';
 // });
 //}
+
 
